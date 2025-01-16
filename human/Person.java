a1c3f5d55e9022b04d98966010f4a04df8478196
Tcoder206 <proglang704206@gmail.com>
2025-01-16 22:19:29 +0700

    First demo of OOP

---
+++ human/Person.java
@@ -0,0 +1,16 @@
+package human;
+
+public class Person {
+    public String name;
+    public int age;
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public void eat(String food) {
+        System.out.println(name + " dang an " + food);
+    }
+    public void getAge() {
+        System.out.println(age);
+    }
+}
---
+++ run.java
@@ -0,0 +1,7 @@
+import human.Person;
+public class run{
+    public static void main(String[] args){
+        Person a = new Person("Nam", 10);
+        a.eat("pizza");
+    }
+}
